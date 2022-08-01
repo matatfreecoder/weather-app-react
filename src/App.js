85f3c2762b71d5e5c8411272aae0bb5c882653ab
Natalia Matat <n.matat@gmail.com>
2022-08-01 10:27:08 +0300

    added footer with git hub link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Wearher App</div>;
+	return (
+		<div className="App">
+			Wearher App
+			<footer>
+				This project was coded by{" "}
+				<a
+					href="https://github.com/matatfreecoder/weather-app-react"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Natalia Matat{" "}
+				</a>
+				and is open-sourse
+			</footer>
+		</div>
+	);
 }
 
 export default App;
