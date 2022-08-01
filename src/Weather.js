6c71aafad7c86f3bb2ac8e334d37b70acc967ccc
Natalia Matat <n.matat@gmail.com>
2022-08-01 18:24:49 +0300

    styling search block

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import "./Weather.css";
 
 export default function Weather() {
 	return (
-		<div className="Weather">
+		<div className="Weather border rounded">
 			<form>
 				<div className="row">
 					<div className="col-8">
@@ -11,10 +11,15 @@ export default function Weather() {
 							type="search"
 							placeholder="Enter a city"
 							className="form-control"
+							autoFocus="on"
 						/>
 					</div>
 					<div className="col-3">
-						<input type="submit" value="Search" className="btn btn-primary" />
+						<input
+							type="submit"
+							value="Search"
+							className="btn btn-primary w-100"
+						/>
 					</div>
 				</div>
 			</form>
@@ -26,21 +31,24 @@ export default function Weather() {
 						alt="Weather icon"
 					/>
 				</div>
-				<div className="col-2">21°C|°F</div>
-				<div className="col-3">
+				<div className="col-2 current-temperature">
+					<span className="temperature">21</span>
+					<span className="units">°C|°F</span>
+				</div>
+				<div className="col-3 current-conditions">
 					<ul>
 						<li>Опади: 17%</li>
 						<li>Вологість: 92%</li>
 						<li>Вітер: 11 км/год</li>
-						<li>Відчувається як </li>
+						<li>Feel like 21°C</li>
 					</ul>
 				</div>
-				<div className="col-5">
+				<div className="col-5 location">
 					<ul>
 						<li>
-							<h1>Нью-Йорк</h1>
+							<h1>Нью-Йорк, США</h1>
 						</li>
-						<li>Сполучені Штати Америки</li>
+
 						<li>понеділок 06:00</li>
 						<li>Хмарно</li>
 					</ul>
